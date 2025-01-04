c3bae71ea45321d7460900765c9c044585d9a6e1
BernhardBaumrock <office@baumrock.com>
2025-01-04 19:56:57 +0100

    add comments

--- examples/01-clock.php
+++ examples/01-clock.php
@@ -6,9 +6,14 @@ use RockSSE\Stream;
 
 rocksse()->addStream(
   url: '/examples/clock',
+
+  // init callback (when stream is initialised)
   init: function (Stream $stream) {
+    // sleep for 1000ms after each loop
     $stream->sleep = 1000;
   },
+
+  // loop callback (called on every iteration)
   loop: function (Stream $stream) {
     $stream->send(date("Y-m-d H:i:s"));
   },
